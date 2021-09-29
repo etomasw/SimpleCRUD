dd8b0d71b5ade9ecfd4d99a6226f2939fe3d58d0
Ernest Tomas <ernest.tomas@urv.cat>
2021-09-29 23:56:29 +0200

    Agregado un comentario.

--- src/main/java/com/ernest/crud/CrudApplication.java
+++ src/main/java/com/ernest/crud/CrudApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudApplication {
 
-	// Added a comment.
+	// Added a comment line.
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
